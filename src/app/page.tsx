bdbe335a11f40ac730b92062b08bbbf220de1336
ImValerio <valeriovallettadev@gmail.com>
2024-02-29 22:01:53 +0100

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between items-center p-24">
+
+      <div>
+        <h1 className="text-6xl title text-white">ProgrammareFacile</h1>
+        <h2 className="text-xl text-white text-right">work in progress...</h2>
+      </div>
+   </main>
+  );
+}
