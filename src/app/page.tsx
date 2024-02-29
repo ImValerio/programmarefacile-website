4c83f76f1408f0c5ff0060021d84a2983393a678
ImValerio <valeriovallettadev@gmail.com>
2024-02-29 22:29:49 +0100

    add tiktok embed section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,16 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between items-center p-24">
-
       <div>
         <h1 className="text-6xl title text-white">ProgrammareFacile</h1>
         <h2 className="text-xl text-white text-right">work in progress...</h2>
-      </div>
+        <blockquote class="tiktok-embed" cite="https://www.tiktok.com/@programmarefacile" data-unique-id="programmarefacile" data-embed-type="creator" > 
+          <section> 
+            <a target="_blank" href="https://www.tiktok.com/@programmarefacile?refer=creator_embed">@programmarefacile</a> 
+          </section> 
+        </blockquote>
+        <script async src="https://www.tiktok.com/embed.js"></script>
+     </div>
    </main>
   );
 }
